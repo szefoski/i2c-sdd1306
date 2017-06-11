543aa9b8dc4985e80cf0b6016d09afb89ca91f1f
szefoski <szefoski22@gmail.com>
2017-06-11 21:22:49 +0200

    added buffer and draw circle

--- i2c_sdd1306.cpp
+++ i2c_sdd1306.cpp
@@ -9,6 +9,8 @@
 #include <string>
 #include <cstring>
 #include <cstdlib>
+#include <bitset>
+#include <cmath>
 
 #define SSD_Command_Mode      0x00  /* C0 and DC bit are 0         */
 #define SSD_Data_Mode         0x40  /* C0 bit is 0 and DC bit is 1 */
@@ -33,11 +35,6 @@ static const uint8_t SSD_Memory_Adressing_Mode_H = 0x00;
 static const uint8_t Column_Max = 127;
 static const uint8_t Page_Max = 7;
 
-struct Pixel_buffer
-{
-    std::array<uint8_t, 128*64/8 + 1> screen_buffer;
-};
-
 class SSD1306
 {
 public:
@@ -46,6 +43,7 @@ public:
         , m_slave_address(slave_address)
     {
         screen_buffer[0] = SSD_Data_Mode;
+        screen_buffer_new[0] = SSD_Data_Mode;
         commands_stack.push_back(SSD_Command_Mode);
     }
 
@@ -133,26 +131,24 @@ public:
         unsigned char buffer[buffer_length] = {SSD_Data_Mode, 0x3C,0x12,0x12,0x12,0x3E,0x00}; 
         send(buffer, buffer_length);
     }
-
+    
     void set_pixel(uint8_t x, uint8_t y, bool value)
     {
-        clean_screen();
-        size_t pixel_no = y * 128 + x;
-        size_t byte_no = y / 8 *128 + x;
-        printf("x(%d), y(%d), no(%d)\n", x, y, byte_no);
-        screen_buffer[byte_no + 1] = 255;
-        flush_buffer();
+        const size_t byte_no = y / 8 *128 + x;
+        std::bitset<8> b(screen_buffer_new[byte_no + 1]);
+        b.set(y%8);
+        screen_buffer_new[byte_no + 1] = (uint8_t)b.to_ulong();
     }
 
     void clean_screen()
     {
-        std::fill(std::begin(screen_buffer) + 1, std::end(screen_buffer), 0);
+        std::fill(std::begin(screen_buffer_new) + 1, std::end(screen_buffer_new), 0);
         flush_buffer();
     }
 
     void draw_random_pixels()
     {
-        for (auto begin = screen_buffer.begin() + 1; begin != screen_buffer.end(); ++begin)
+        for (auto begin = screen_buffer_new.begin() + 1; begin != screen_buffer_new.end(); ++begin)
         {
             *begin = rand() % 256; 
         }
@@ -165,10 +161,110 @@ public:
         flush_commands();
     };
 
+    void draw_circle2(float x1,float y1,float r)
+    {
+        float x,y,p;
+        x=0;
+        y=r;
+        p=3-(2*r);
+        while(x<=y)
+        {
+            set_pixel(x1+x,y1+y,true);
+            set_pixel(x1-x,y1+y,true);
+            set_pixel(x1+x,y1-y,true);
+            set_pixel(x1-x,y1-y,true);
+            set_pixel(x1+y,y1+x,true);
+            set_pixel(x1+y,y1-x,true);
+            set_pixel(x1-y,y1+x,true);
+            set_pixel(x1-y,y1-x,true);
+            x=x+1;
+            if(p<0)
+            {
+                p=p+4*(x)+6;
+            }
+            else
+            {
+                p=p+4*(x-y)+10;
+                y=y-1;
+            }
+        }
+    }
+
+    void draw_filled_circle(int x0, int y0, int radius)
+    {
+        int x = radius;
+        int y = 0;
+        int xChange = 1 - (radius << 1);
+        int yChange = 0;
+        int radiusError = 0;
+
+        while (x >= y)
+        {
+            for (int i = x0 - x; i <= x0 + x; i++)
+            {
+                set_pixel(i, y0 + y, true);
+                set_pixel(i, y0 - y, true);
+            }
+            for (int i = x0 - y; i <= x0 + y; i++)
+            {
+                set_pixel(i, y0 + x, true);
+                set_pixel(i, y0 - x, true);
+            }
+
+            y++;
+            radiusError += yChange;
+            yChange += 2;
+            if (((radiusError << 1) + xChange) > 0)
+            {
+                x--;
+                radiusError += xChange;
+                xChange += 2;
+            }
+        }
+    }
+
     void flush_buffer()
     {
-        send(screen_buffer.data(), screen_buffer.size());
-    };
+        size_t no_of_diffs = 0;
+        size_t min_column = Column_Max;
+        size_t max_column = 0;
+        size_t min_page = Page_Max;
+        size_t max_page = 0;
+
+        for (size_t column = 0; column <= Column_Max; ++column)
+        {
+            for (size_t page = 0; page <= Page_Max; ++page)
+            {
+                if (get_byte(column, page, screen_buffer_new) != get_byte(column, page, screen_buffer))
+                {
+                    min_column = std::min(min_column, column);
+                    max_column = std::max(max_column, column);
+                    min_page = std::min(min_page, page);
+                    max_page = std::max(max_page, page);
+                    ++no_of_diffs;
+                }
+            }
+        }
+
+        const size_t area = (max_column - min_column + 1) * (max_page - min_page + 1);
+        std::vector<uint8_t> vec(area + 1);
+        vec[0] = SSD_Data_Mode;
+        size_t counter = 0;
+        
+        for (size_t page = min_page; page <= max_page; ++page)
+        {
+            for (size_t column = min_column; column <= max_column; ++column)
+            { 
+                vec[counter + 1] = get_byte(column, page, screen_buffer_new);
+                ++counter; 
+            }
+        }
+
+        set_draw_area(min_column, max_column, min_page, max_page);
+        printf("Area: %u\nmin_c(%u) max_c(%u)\nmin_p(%u) max_p(%u)\n", area, min_column, max_column, min_page, max_page);
+        send(vec);
+        screen_buffer = screen_buffer_new;
+    }
 
     void flush_commands()
     {
@@ -181,6 +277,11 @@ public:
         commands_stack.insert(std::end(commands_stack), list);
     }
 private:
+    const uint8_t& get_byte(uint8_t column, uint8_t page, const std::array<uint8_t, 128*64/8 + 1> &buffer)
+    {
+       return buffer[page * 128 + column + 1]; 
+    }
+
     void send(const std::vector<uint8_t> &buffer)
     {
         send(buffer.data(), buffer.size());
@@ -250,7 +351,27 @@ int main(int argc, char *argv[])
     }
     else if (strcmp(argv[1], "d_sp") == 0)
     {
+        oled.clean_screen();
         oled.set_pixel(static_cast<uint8_t>(strtol(argv[2], NULL, 10)), static_cast<uint8_t>(strtol(argv[3], NULL, 10)), true);
+        oled.flush_buffer();
+    }
+    else if (strcmp(argv[1], "d_circle") == 0)
+    {
+        oled.clean_screen();
+        const uint8_t x = static_cast<uint8_t>(strtol(argv[2], NULL, 10));
+        const uint8_t y = static_cast<uint8_t>(strtol(argv[3], NULL, 10));
+        const uint8_t r = static_cast<uint8_t>(strtol(argv[4], NULL, 10));
+        oled.draw_circle2(x, y, r);
+        oled.flush_buffer();
+    }
+    else if (strcmp(argv[1], "d_circle_f") == 0)
+    {
+        oled.clean_screen();
+        const uint8_t x = static_cast<uint8_t>(strtol(argv[2], NULL, 10));
+        const uint8_t y = static_cast<uint8_t>(strtol(argv[3], NULL, 10));
+        const uint8_t r = static_cast<uint8_t>(strtol(argv[4], NULL, 10));
+        oled.draw_filled_circle(x, y, r);
+        oled.flush_buffer();
     }
     else if (strcmp(argv[1], "d") == 0)
     {
